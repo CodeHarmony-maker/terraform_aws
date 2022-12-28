ea704a3d54f4d7d692bf940e4908930bb2c6ef02
nazakatalidevops <nazakatalikharal786@gmail.com>
2022-12-28 18:44:13 +0500

    create environment variables

--- main.tf
+++ main.tf
@@ -1,11 +1,25 @@
 provider "aws" {
-    region = "eu-west-3"
-    access_key = ""
-    secret_key = ""
+    # region = "eu-west-3"
+    # access_key = ""
+    # secret_key = ""
 }
 
 variable "subnet_cidr_block" {
   description = "subnet_cidr_block"
+  default = "10.0.10.0/24"
+}
+
+variable "cidr_blocks" {
+    description = "cidr block for vpc and subnets"
+    type = list(string)
+}
+
+variable "cidr_block_objects" {
+    description = "cidr block and name tags for vpc and subnets"
+    type = list(object({
+        cidr_block = string
+        name = string
+    }))
 }
 
 variable "vpc_cidr_block" {
@@ -16,11 +30,39 @@ variable "environment" {
     description = "deployment environment"
 }
 
+variable "avail_zone" {}
+
+resource "aws_vpc" "development-vpc-object" { 
+    cidr_block = var.cidr_block_objects[0].cidr_block
+    tags = {
+        Name: var.cidr_block_objects[0].name
+        vpc-env: "dev"
+    }
+}
+
+resource "aws_subnet" "dev-subnet-object" {
+    vpc_id = aws_vpc.development-vpc.id
+    cidr_block = var.cidr_block_objects[1].cidr_block
+    availability_zone = var.avail_zone
+    tags = {
+        Name: var.cidr_block_objects[10].name
+    }
+}
+
 resource "aws_vpc" "development-vpc" { 
-    cidr_block = var.vpc_cidr_block
+    cidr_block = var.cidr_block[0]
     tags = {
         Name: "development",
         vpc-env: "dev"
+    }
+}
+
+resource "aws_subnet" "dev-subnet-0" {
+    vpc_id = aws_vpc.development-vpc.id
+    cidr_block = var.cidr_block[1]
+    availability_zone = "es-west-3a"
+    tags = {
+        Name: "subnet-0-dev"
     }
 }
 
--- terraform-dev.tfvars
+++ terraform-dev.tfvars
@@ -1,4 +1,8 @@
-subnet_cidr_block = "10.0.40.0/24"
+subnet_cidr_block = "10.0.50.0/24"
 vpc_cidr_block = "10.0.0.0/16"
-
 environment = "development"
+cidr_block = ["10.0.0.0/16", "10.0.10.0/24"]
+cidr_block_objects = [
+    {cidr_block =  "10.0.0.0/16", name = "dev-vpc"}
+    {cidr_block =  "10.0.10.0/24", name = "dev-subnet"}
+]
